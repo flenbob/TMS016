13817eb2a5752c748e904ff9d2ff25ded0ad6a44
flenbob <ivan.flensburg@gmail.com>
2022-04-25 14:44:55 +0200

    fix

--- project 1/main.m
+++ project 1/main.m
@@ -1,9 +1,11 @@
+clear 
+clf
+close all
 %Load titan images in grayscale 0-1 format and plot
-clear
 img_titan = double(imread("titan.jpg"))/255;
-img_rosetta = double(imread("rosetta.jpg"))/255;
+img_rosetta = double(rgb2gray(imread("rosetta.jpg")))/255;
 
-figure(1)
+figure
 subplot(1,2,1)
 imagesc(img_titan)
 title('Titan')
@@ -13,102 +15,109 @@ imagesc(img_rosetta)
 title('Rosetta')
 colormap 'gray'
 
-%Generate data of observed points and ones to predict
-p_c = 0.49;
-[x_o, x_m, ind_o, ind_m] = generate_data(img_titan, p_c);
-
-%Generate mesh of all possible locations
-[m, n] = size(img_titan);
-[X, Y] = meshgrid(1:m, 1:n);
-loc = [X(:), Y(:)];
-
-%Get observed and missing locations:
-loc_o = loc(ind_o, :);
-loc_m = loc(ind_m, :);
-
-%Parameters for gauss field with matern covariance
-sz = 2000; %set size for faster computation
-
-%Covariates (intercept + horizontal + vertical)
-%Observed pixels: 
-B0_o = ones(size(x_o));
-BX_o = loc_o(:,1);
-BY_o = loc_o(:,2);
-cov_o = [B0_o BX_o BY_o];
-
-%Missing pixels:
-B0_m = ones(size(x_m));
-BX_m = loc_m(:,1);
-BY_m = loc_m(:,2);
-cov_m = [B0_m BX_m BY_m];
-
-%Least squares estimate and means
-LSE = (cov_o'*cov_o)\cov_o'*x_o;
-mu_o = cov_o*LSE;
-mu_m = cov_m*LSE;
-
-%Residuals of observed pixels:
-e_o = x_o(1:sz) - mu_o(1:sz);
-
-%Fit variogram to residual of observed pixels to estimate parameters
-emp_v = emp_variogram(loc_o(1:sz, :), e_o, 100);
-lse = cov_ls_est(e_o, 'matern', emp_v, struct('nu', 1));
-
-%Plot variogram
-mat_v = matern_variogram(emp_v.h, lse.sigma, lse.kappa, lse.nu, lse.sigma_e);
-
-figure(2)
-plot(emp_v.h,mat_v)
-legend('observed pixels variogram')
-hold off
-
-%% GMRFs
-
-%Stencils:
-q1 = [0 0 0 0 0; 
-      0 0 0 0 0;
-      0 0 1 0 0;
-      0 0 0 0 0;
-      0 0 0 0 0];
-
-q2 = [0 0 0 0 0;
-      0 0 -1 0 0;
-      0 -1 4 -1 0;
-      0 0 -1 0 0;
-      0 0 0 0 0];
-
-q3 = [0 0 1 0 0;
-      0 2 -8 2 0;
-      1 -8 20 -8 1;
-      0 2 -8 2 0;
-      0 0 1 0 0];
-
-tau = 2*pi/lse.sigma^2;
-Q = @(kappa) tau*stencil2prec([m, n], kappa^4*q1 + 2*kappa^2*q2 + q3);
-
-%Generate reconstructed image with estimated kappa
-Recon_image = recon_img(Q, lse.kappa, ind_o, ind_m, mu_o, mu_m, x_o);
-
-%Plot reconstructed image, original image and differences
-figure(3)
-subplot(1,3,1)
-imagesc(reshape(Recon_image,[m,n]));
-title("Reconstructed Image");
-axis image
-
-subplot(1,3,2)
-imagesc(img_titan);
-title("Original Image")
-axis image
-
-subplot(1,3,3)
-imagesc(reshape(Recon_image,[m,n]) - img_titan);
-title("Differences")
-axis image
-colormap 'gray'
+p_c = 0.01;
+sz = 2000;
+reconstruction(img_titan, p_c, sz)
+reconstruction(img_rosetta, p_c, sz)
 
 %% FUNCTIONS
-function Recon_image = recon_img(Q, kappa, ind_o, ind_m, mu_o, mu_m, x_o)
+function reconstruction(img, p_c, sz)
+    %Generate data of observed points and ones to predict
+    [x_o, x_m, ind_o, ind_m] = generate_data(img, p_c);
+    
+    %Generate mesh of all possible locations
+    [m, n] = size(img);
+    [X, Y] = meshgrid(1:m, 1:n);
+    loc = [X(:), Y(:)];
+
+    %If subset size is larger than amount of observed pixels, correct:
+    if sz > length(x_o)
+        sz = length(x_o);
+    end
+
+    %Get observed and missing locations:
+    loc_o = loc(ind_o, :);
+    loc_m = loc(ind_m, :);
+    
+    %Covariates (intercept + horizontal + vertical)
+    %Observed pixels: 
+    B0_o = ones(size(x_o));
+    BX_o = loc_o(:,1);
+    BY_o = loc_o(:,2);
+    cov_o = [B0_o BX_o BY_o];
+    
+    %Missing pixels:
+    B0_m = ones(size(x_m));
+    BX_m = loc_m(:,1);
+    BY_m = loc_m(:,2);
+    cov_m = [B0_m BX_m BY_m];
+    
+    %Least squares estimate and means
+    LSE = (cov_o'*cov_o)\cov_o'*x_o;
+    mu_o = cov_o*LSE;
+    mu_m = cov_m*LSE;
+    
+    %Residuals of observed pixels:
+    e_o = x_o(1:sz) - mu_o(1:sz);
+    
+    %Fit variogram to residual of observed pixels to estimate parameters
+    emp_v = emp_variogram(loc_o(1:sz, :), e_o, 100);
+    lse = cov_ls_est(e_o, 'matern', emp_v, struct('nu', 1));
+    
+    %Plot variogram
+%     mat_v = matern_variogram(emp_v.h, lse.sigma, lse.kappa, lse.nu, lse.sigma_e);    
+%     figure(2)
+%     plot(emp_v.h,mat_v)
+%     legend('observed pixels variogram')
+%     hold off
+    
+    %% GMRFs
+    
+    %Stencils:
+    q1 = [0 0 0 0 0; 
+          0 0 0 0 0;
+          0 0 1 0 0;
+          0 0 0 0 0;
+          0 0 0 0 0];
+    
+    q2 = [0 0 0 0 0;
+          0 0 -1 0 0;
+          0 -1 4 -1 0;
+          0 0 -1 0 0;
+          0 0 0 0 0];
+    
+    q3 = [0 0 1 0 0;
+          0 2 -8 2 0;
+          1 -8 20 -8 1;
+          0 2 -8 2 0;
+          0 0 1 0 0];
+    
+    tau = 2*pi/lse.sigma^2;
+    Q = @(kappa) tau*stencil2prec([m, n], kappa^4*q1 + 2*kappa^2*q2 + q3);
+    
+    %Generate reconstructed image with estimated kappa
+    Recon_image = krig_img(Q, lse.kappa, ind_o, ind_m, mu_o, mu_m, x_o);
+    
+    %Plot reconstructed image, original image and differences
+    figure
+    subplot(1,3,1)
+    imagesc(reshape(Recon_image,[m,n]));
+    title("Reconstructed Image");
+    axis image
+    
+    subplot(1,3,2)
+    imagesc(img);
+    title("Original Image")
+    axis image
+    
+    subplot(1,3,3)
+    imagesc(reshape(Recon_image,[m,n]) - img);
+    title("Differences")
+    axis image
+    colormap 'gray'
+end
+
+function Recon_image = krig_img(Q, kappa, ind_o, ind_m, mu_o, mu_m, x_o)
     %Use estimated parameters to generate GMRF:
     Q = Q(kappa);
 
