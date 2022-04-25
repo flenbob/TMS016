26802ba62c89d32f02a874efd4cc9164c761d656
flenbob <ivan.flensburg@gmail.com>
2022-04-25 14:15:05 +0200

    big fix

--- project 1/main.m
+++ project 1/main.m
@@ -28,53 +28,38 @@ loc_m = loc(ind_m, :);
 
 %Parameters for gauss field with matern covariance
 sz = 2000; %set size for faster computation
-nu = 1;
-sigma = 1;
-kappa = 1;
-
-Xs = generate_gauss_field(nu, sigma, kappa, loc(1:sz, :));
 
 %Covariates (intercept + horizontal + vertical)
-%Observed values: 
+%Observed pixels: 
 B0_o = ones(size(x_o));
 BX_o = loc_o(:,1);
 BY_o = loc_o(:,2);
 cov_o = [B0_o BX_o BY_o];
 
-%Missing values:
+%Missing pixels:
 B0_m = ones(size(x_m));
 BX_m = loc_m(:,1);
 BY_m = loc_m(:,2);
 cov_m = [B0_m BX_m BY_m];
 
-%Least squares estimate
+%Least squares estimate and means
 LSE = (cov_o'*cov_o)\cov_o'*x_o;
 mu_o = cov_o*LSE;
 mu_m = cov_m*LSE;
 
-%Predicted values for missing pixels
-Y_hat = mu_m(1:sz) + Xs;
+%Residuals of observed pixels:
+e_o = x_o(1:sz) - mu_o(1:sz);
 
-%Get residuals (observed vs predicted values):
-e = x_o(1:sz) - Y_hat;
+%Fit variogram to residual of observed pixels to estimate parameters
+emp_v = emp_variogram(loc_o(1:sz, :), e_o, 100);
+lse = cov_ls_est(e_o, 'matern', emp_v, struct('nu', 1));
 
-%Bin residuals and fit variogram
-h = linspace(0, n, 100);
-mat_v = matern_variogram(h, sigma, kappa, nu, 0); %True vs binned estimate of the variogram
-
-%Estimate variogram
-emp_v = emp_variogram(loc(1:sz, :), e, 500);
-
-lse = cov_ls_est(e, 'matern', emp_v);
-
-mat_v_new = matern_variogram(h, lse.sigma, lse.kappa, ...
-                            lse.nu, lse.sigma_e);
+%Plot variogram
+mat_v = matern_variogram(emp_v.h, lse.sigma, lse.kappa, lse.nu, lse.sigma_e);
 
 figure(2)
-plot(h,mat_v)
-hold on
-plot(emp_v.h,emp_v.variogram,'.')
-plot(h,mat_v_new)
+plot(emp_v.h,mat_v)
+legend('observed pixels variogram')
 hold off
 
 %% GMRFs
@@ -101,44 +86,8 @@ q3 = [0 0 1 0 0;
 tau = 2*pi/lse.sigma^2;
 Q = @(kappa) tau*stencil2prec([m, n], kappa^4*q1 + 2*kappa^2*q2 + q3);
 
-%Optimize kappa value. Initialize parameters:
-Recon_image = recon_img(Q, kappa, ind_o, ind_m, mu_o, mu_m, x_o);
-prev_error = sum(abs(Recon_image - reshape(img_titan, [m*n, 1])));
-error = 0;
-
-kappa = lse.kappa;
-alpha = 3; %step scaling factor
-h = 1e-2; %step to determine derivative
-img_array = reshape(img_titan, [m*n, 1]); 
-
-while prev_error > error 
-    %Get two image estimates with 'kappa' and 'kappa+h' to determine
-    %descent direction
-    Recon_image = recon_img(Q, kappa, ind_o, ind_m, mu_o, mu_m, x_o);
-    Recon_image_h = recon_img(Q, kappa+h, ind_o, ind_m, mu_o, mu_m, x_o);
-
-    %Differences 
-    diff = sum(abs(Recon_image - img_array));
-    diff_h = sum(abs(Recon_image_h - img_array));
-    
-    %Derivative
-    df = (diff_h-diff)/h;
-
-    %Update new kappa and save previous in case of optimality:
-    kappa_prev = kappa
-    kappa = kappa - alpha/df;
-
-    %Save error for next iteration:
-    prev_error = sum(abs(Recon_image - reshape(img_titan, [m*n, 1])))
-
-    %Calculate error from new kappa:
-    Recon_image = recon_img(Q, kappa, ind_o, ind_m, mu_o, mu_m, x_o);
-    error = sum(abs(Recon_image - reshape(img_titan, [m*n, 1])));
-end
-
-%Generate reconstructed image with 'optimal' kappa
-kappa = kappa_prev;
-Recon_image = recon_img(Q, kappa, ind_o, ind_m, mu_o, mu_m, x_o);
+%Generate reconstructed image with estimated kappa
+Recon_image = recon_img(Q, lse.kappa, ind_o, ind_m, mu_o, mu_m, x_o);
 
 %Plot reconstructed image, original image and differences
 figure(3)
@@ -173,20 +122,6 @@ function Recon_image = recon_img(Q, kappa, ind_o, ind_m, mu_o, mu_m, x_o)
     Recon_image = zeros(length(ind_o) + length(ind_m), 1);
     Recon_image(ind_o) = x_o;
     Recon_image(ind_m) = krig;
-end
-
-function Z = generate_gauss_field(nu, sigma, kappa, loc)
-    %Given location matrix and parameters, generate a 
-    %Gaussian field with a Matérn covariance
-    D = squareform(pdist(loc));
-    ma_co = matern_covariance(D, sigma, kappa, nu);
-    R = chol(ma_co);
-    
-    %Randomize normally distributed z ~ N(0,1)
-    z = randn(size(loc, 1), 1);
-    
-    %Return zero mean Gaussian random field:
-    Z = R*z;
 end
 
 function [x_o, x_m, ind_o, ind_m] = generate_data(img, p)
