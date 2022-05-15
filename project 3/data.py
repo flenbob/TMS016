f1c08b77766e3a423ffac3b3186558d315b59ddd
flenbob <ivan.flensburg@gmail.com>
2022-05-15 15:44:48 +0200

    change

--- project 3/data.py
+++ project 3/data.py
@@ -1,33 +1,74 @@
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
+import matplotlib
 from math import sqrt, sin, cos
 import cv2
 import os
+from PIL import Image, ImageOps
 
 def main():
     #Create face images of dim pixels
     #Copy face_coords.txt and place in same dir as folders '2002' and '2003'
     #where face_coords.txt are all .txt files concatenated
-    path_read = '../FDDB/'
+    path_read = '../FDDB'
     path_write = '../FDDB/faces'
     dim = (24,24)
-    generate_face_data(path_read, path_write, dim)
-    
-def generate_face_data(path_read: str, path_write: str, dim: tuple[int, int]) -> None:
+    #positive_images(path_read, path_write, dim)
+    negative_images('../negative_imgs/images', '../neg_imgs', dim)
+
+
+def negative_images(path_read: str, path_write: str, dim: tuple[int, int]) -> None:
+    #Crop 2 square regions from a given image and then resize to 24x24
+    for image_path in os.listdir(path_read):
+        print(f'{path_read}/{image_path}')
+        #img = cv2.imread(f'{path_read}/{image_path}', 0)
+        img = Image.open(f'{path_read}/{image_path}')
+        img = ImageOps.grayscale(img)
+        w, h = img.size
+        img_1 = img.crop((0, 0, int(w/2), int(h/2)))
+        img_2 = img.crop((int(w/2), int(h/2), w, h))
+        
+        norm_1 = np.zeros(dim)
+        norm_2 = np.zeros(dim)
+        
+        img_1 = img_1.resize(dim, Image.ANTIALIAS)
+        img_2 = img_2.resize(dim, Image.ANTIALIAS)
+        img_1 = np.array(img_1)
+        img_2 = np.array(img_2)
+        
+        img_1 = cv2.normalize(img_1, norm_1, 0, 255, cv2.NORM_MINMAX)
+        img_2 = cv2.normalize(img_2, norm_2, 0, 255, cv2.NORM_MINMAX)
+
+        cv2.imwrite(f'{path_write}/{image_path}_{1}.png', img_1)
+        cv2.imwrite(f'{path_write}/{image_path}_{2}.png', img_2)
+
+        #img_1.save(f'{path_write}/{image_path}_{1}.png')
+        #img_2.save(f'{path_write}/{image_path}_{2}.png')
+        
+        #img_1_crop = img[0:sz, 0:sz]
+        #img_2_crop = img[sz:2*sz, sz:2*sz]
+        #new_1_img = cv2.resize(img_1_crop, dim, interpolation = cv2.INTER_AREA)
+        #new_2_img = cv2.resize(img_2_crop, dim, interpolation = cv2.INTER_AREA)
+        #cv2.imwrite(f'{path_write}/{image_path}_{1}.png', new_1_img)
+        #cv2.imwrite(f'{path_write}/{image_path}_{2}.png', new_2_img)
+
+def positive_images(path_read: str, path_write: str, dim: tuple[int, int]) -> None:
     # Read text document in filepath path_read
     # for each image:
     #   for each face:
     #       crop face, resize to dim pixels and save to filepath path_write
     
-    face_coords = open(path_read+'face_coords.txt','r')
+    face_coords = open(f'{path_read}/face_coords.txt','r')
     lines = face_coords.readlines()
     for idx, line in enumerate(lines):
         #If image on current line
         if line[0:4] == ('2002' or '2003'):
-            path = path_read+line[:-1]+'.jpg'
+            path = f'{path_read}/{line[:-1]}.jpg'
             img = cv2.imread(path, 0)
             n_faces = int(lines[idx+1])
+            if n_faces > 3: #If too many faces in same image, then skip it (bad results in those cases)
+                continue
             masks = lines[idx+2:idx+2+n_faces]
             img_id = line[line.index('_')+1:-1]
         
@@ -36,22 +77,25 @@ def generate_face_data(path_read: str, path_write: str, dim: tuple[int, int]) ->
                 masks[idx] = [float(i) for i in mask.split()[:-1]]
                 x, y, w, h = ellipse2rect(masks[idx])
                 img_crop = img[y:y+h, x:x+w]
-                new_img = cv2.resize(img_crop, dim)
+                new_img = cv2.resize(img_crop, dim, interpolation = cv2.INTER_AREA)
                 cv2.imwrite(f'{path_write}/face_img_{img_id}_{idx}.png', new_img)
          
 def ellipse2rect(ellipse_mask):
     #Convert ellipse mask to rectangle
     a, b, theta, x_o, y_o = ellipse_mask
+    
     x = sqrt(a**2*cos(theta)**2+b**2*sin(theta)**2)
     y = sqrt(a**2*sin(theta)**2+b**2*cos(theta)**2)
+    m = (x+y)/2
     
     #Get top left (x,y) and width, height (w, h)
-    x_tl = int(abs(x_o-x))
-    y_tl = int(abs(y_o-y))
-    w = int(2*x)
-    h = int(2*y)
+    x_tl = int(abs(x_o-m))
+    y_tl = int(abs(y_o-m))
+    w = int(2*m)
+    h = int(2*m)
     
     return x_tl, y_tl, w, h
+
     
 if __name__ == "__main__":
     main()
--- project 3/main.py
+++ project 3/main.py
@@ -9,36 +9,52 @@ import pandas as pd
 import cv2
 import os
 from os import listdir
+from numpy import genfromtxt
 from sklearn.model_selection import train_test_split
+from sklearn.ensemble import RandomForestClassifier
 
 def main():
     n_f = 162336
-    n_img = 4495
-    img_type = 'positive'
-    path_read = '../FDDB/faces'
-    path_write = '../FDDB'
-    img2haar_features(path_read, path_write, n_f, n_img, img_type)
     
-def img2haar_features(path_read, path_write, n_f, n_img, img_type):
-    #Get haar features for each of the 24 x 24 images (1 = positive, 0 = negative)
-    # and save to h5 file
-    if img_type == 'positive':
-        cl = 1
-    elif img_type == 'negative':
-        cl = 0
-    else:
-        return print('img_type must be either ''positive'' or ''negative''')
+    #Load haar features for negative and positive images
+    X_pos, y_pos = img2haar_features('../FDDB/test_pos', n_f, 5, 1)
+    X_neg, y_neg = img2haar_features('../FDDB/test_neg', n_f, 5, 0)
     
-    haar_f = np.empty((n_img,n_f), float)
+    #Concatenate data and split into test and train data
+    X = np.concatenate((X_pos, X_neg))
+    y = np.concatenate((y_pos, y_neg))
+    X_train, X_test, y_train, y_test = train_test_split(X, y, train_size=0.25,
+                                                    random_state=0,
+                                                    stratify=y)
+    
+    #Use forest classifier to reduce the number of features
+    clf = RandomForestClassifier(n_estimators=1000, max_depth=None,
+                             max_features=100, n_jobs=-1, random_state=0)
+    
+    #Fit model to training data and then score the test data
+    clf.fit(X_train, y_train)
+    print(clf.score(X_test, y_test))
+    
+    
+def img2haar_features(path_read: str, n_f: int, n_img: int, img_type: int) -> np.ndarray:
+    #Get haar features for each image (1 = positive, 0 = negative)
+    
+    #X: Haar feature for each image
+    #y: label for each image
+    X = np.empty((n_img, n_f))
+    y = np.array([img_type]*n_img)
+    
+    #Calculate haar feature and break loop if out of bounds
     for idx, image_id in enumerate(os.listdir(path_read)):
-        img = cv2.imread(path_read+image_id, 0)
+        print(idx)
+        img = cv2.imread(f'{path_read}/{image_id}', 0)
         img_ii = integral_image(img)
         feature = haar_like_feature(img_ii, 0, 0, 24, 24)
-        haar_f[idx] = feature
+        X[idx] = feature
+        if idx + 1 == n_img: 
+            break
         
-    df = pd.DataFrame(haar_f)
-    df['class'] = cl
-    df.to_hdf(f'{path_write}/{img_type}.h5', key=img_type, mode='w')
+    return X, y
 
 if __name__ == "__main__":
     main()
