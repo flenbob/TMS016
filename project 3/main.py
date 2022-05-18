e4c0f483666dc971a00a05fe951447c0ec32187e
flenbob <ivan.flensburg@gmail.com>
2022-05-18 13:37:40 +0200

    data is overfitting

--- project 3/main.py
+++ project 3/main.py
@@ -1,7 +1,5 @@
-from typing import Any
 from skimage.feature import haar_like_feature
 from skimage.transform import integral_image
-import matplotlib.pyplot as plt
 import numpy as np
 import cv2
 import os
@@ -11,24 +9,48 @@ from sklearn.tree import DecisionTreeClassifier
 from sklearn.metrics import roc_curve
 from time import time
 import h5py
-import pandas as pd
 import pickle
-from sklearn.metrics import confusion_matrix, ConfusionMatrixDisplay
-import math
+from sklearn.metrics import confusion_matrix
 
 def main():
     T = [1, 5, 25, 25, 50, 50, 75]
     FNR_thresh = 0.1
     
-    X_pos, y_pos = read_hdf5('data/positive_test.hdf5')
-    X_neg, y_neg = read_hdf5('data/negative_test.hdf5')
-    X = np.concatenate((X_pos, X_neg))
-    y = np.concatenate((y_pos, y_neg))
+    X_pos, y_pos = read_feature_file('data/positive_test.hdf5')
+    #X_neg, y_neg = read_feature_file('data/negative_test.hdf5')
+    #X = np.concatenate((X_pos, X_neg))
+    #y = np.concatenate((y_pos, y_neg))
     print('Files loaded')
-    file_path = 'data/adaboost_classifiers_test.pkl'
-    train_adaboost_classifiers(FNR_thresh, T, X, y, file_path)
+    file_path = 'data/adaboost_classifiers.pkl'
+    models = read_model_file(file_path)
+    
+    label = []
+    for row in X_pos:
+        label.append(cascade_classifier(row, models))
+        
+    print(label)
+    print(sum(label)/len(label))
+    #train_adaboost_classifiers(FNR_thresh, T, X, y, file_path)
     
     
+def cascade_classifier(img: np.ndarray, models: list) -> int:
+    # Input:
+    # features: Array of features for an image
+    # models: List of models for each layer in the cascade
+    
+    # Returns:
+    # 1 if all layers predicts over the set threshold
+    # 0 otherwise
+    for model in models:
+        #Load strong classifier and its threshold
+        img = img.reshape(1,-1)
+        SC = model[0]
+        threshold = model[1]
+        y_pred = 1*(SC.predict_proba(img)[:,1] >= threshold)
+        if y_pred == 0:
+            return 0
+    return 1
+
 def train_adaboost_classifiers(FNR_thresh: float, T: list, X: np.ndarray, y: np.ndarray, file_path: str) -> None:
     # Input:
     # FNR_thresh: Maximum False Negative Ratio threshold [0, 1]
@@ -46,62 +68,61 @@ def train_adaboost_classifiers(FNR_thresh: float, T: list, X: np.ndarray, y: np.
     FP = 1
     
     #Open file
-    file = open(file_path, 'wb')
-    while (TN and FN and FP) != 0:
-        for t in T:
-            print(f'Training SC with {t} WC:s....')
-            #SC with t WC:s
-            clf = AdaBoostClassifier(
-                DecisionTreeClassifier(criterion='gini', max_depth=1), 
-                algorithm="SAMME.R", 
-                n_estimators=t
-            )
-            #Train SC
-            clf.fit(X, y)
-            y_pred = clf.predict(X)
+    with open(file_path, 'wb') as f:
+        while (TN and FN and FP) != 0:
+            for t in T:
+                print(f'Training SC with {t} WC:s....')
+                #SC with t WC:s
+                clf = AdaBoostClassifier(
+                    DecisionTreeClassifier(criterion='gini', max_depth=1), 
+                    algorithm="SAMME.R", 
+                    n_estimators=t
+                )
+                #Train SC
+                clf.fit(X, y)
+                y_pred = clf.predict(X)
+                
+                #Find probability threshold to reduce FPR to 1%
+                print(np.shape(y_pred))
+                TN, FP, FN, TP = confusion_matrix(y, y_pred, labels=[0,1]).ravel()
+                FNR = FN/(FN+TP)
+                
+                #If only TP samples remains
+                thresh = 0.5
+                if (FP and FN and TN) == 0:
+                    pickle.dump([clf, thresh], f)
+                    break
             
-            #Find probability threshold to reduce FPR to 1%
-            print(np.shape(y_pred))
-            TN, FP, FN, TP = confusion_matrix(y, y_pred, labels=[0,1]).ravel()
-            FNR = FN/(FN+TP)
+                print(f'Before thresh, FNR: {FNR}')
+                #If probability threshold is not satisfied
+                if FNR > FNR_thresh:
+                    y_prob = clf.predict_proba(X)[:, 1]
+                    FPR_list, TPR_list, threshold_list = roc_curve(y, y_prob)
+                    FNR_list = 1 - TPR_list
+                    thresh = threshold_FNR(FNR_list, threshold_list, FNR_thresh)
+                
+                #Predict labels with new threshold
+                y_pred = 1*(clf.predict_proba(X)[:,1] >= thresh)
+                print(f'New threshold: {thresh}')
             
-            #If only TP samples remains
-            if (FP and FN and TN) == 0:
-                break
-        
-            print(f'Before thresh, FNR: {FNR}')
-            #If probability threshold is not satisfied
-            thresh = 0.5
-            if FNR > FNR_thresh:
-                y_prob = clf.predict_proba(X)[:, 1]
-                FPR_list, TPR_list, threshold_list = roc_curve(y, y_prob)
-                FNR_list = 1 - TPR_list
-                thresh = threshold_FNR(FNR_list, threshold_list, FNR_thresh)
+                #Remove samples that are TN
+                TN_idx = []
+                for i in range(len(y_pred)):
+                    if (y[i] == 0) and (y_pred[i] == 0):
+                        TN_idx.append(i)
             
-            #Predict labels with new threshold
-            y_pred = 1*(clf.predict_proba(X)[:,1] >= thresh)
-            print(f'New threshold: {thresh}')
-        
-            #Remove samples that are TN
-            TN_idx = []
-            for i in range(len(y_pred)):
-                if (y[i] == 0) and (y_pred[i] == 0):
-                    TN_idx.append(i)
-        
-            X = np.delete(X, TN_idx, axis=0)
-            y = np.delete(y, TN_idx)
-            y_pred = np.delete(y_pred, TN_idx)
-            
-            #Check confusion matrix
-            TN, FP, FN, TP = confusion_matrix(y, y_pred, labels=[0,1]).ravel()
-            print(f'TN: {TN}, FP: {FP}, FN: {FN}, TP: {TP}')
-            #Append SC and threshold to .pkl file
-            pickle.dump([clf, thresh], file)
-            
-    file.close()
+                X = np.delete(X, TN_idx, axis=0)
+                y = np.delete(y, TN_idx)
+                y_pred = np.delete(y_pred, TN_idx)
+                
+                #Check confusion matrix
+                TN, FP, FN, TP = confusion_matrix(y, y_pred, labels=[0,1]).ravel()
+                print(f'TN: {TN}, FP: {FP}, FN: {FN}, TP: {TP}')
+                #Append SC and threshold to .pkl file
+                pickle.dump([clf, thresh], f)
+                
     print(f'Saved all SC:s and thresholds to: {file_path}')
-    
-    
+        
 def threshold_FNR(FNR_list: np.ndarray, threshold_list: np.ndarray, target_FNR: float) -> float:
     #Find threshold that satisfies target false negative rate (FNR)
     idx = 0
@@ -124,6 +145,29 @@ def threshold_FNR(FNR_list: np.ndarray, threshold_list: np.ndarray, target_FNR: 
     target_thresh = left_threshold-(ratio*(left_threshold-right_threshold))
     return target_thresh
 
+def read_model_file(file_path:str) -> list:
+    models = []
+    with open(file_path, 'rb') as f:
+        try:
+            while True:
+                model = pickle.load(f)
+                print(f'{model}')
+                models.append(model)
+        except (EOFError):
+            pass
+    return models   
+   
+def read_feature_file(path_read: str):
+    #Read feature hdf5 file and return:
+    #X: features for each image
+    #y: labels for each image 
+    file = h5py.File(f'{path_read}', 'r')
+    X = file['features'] 
+    y = file['labels']
+    X = np.array(X)
+    y = np.array(y)
+    file.close()
+    return X, y
 
 def img2haar_features(path_read: str, path_write: str, n_f: int, n_img: int, img_type: str) -> np.ndarray:
     #X: Haar feature for each image
@@ -152,18 +196,6 @@ def img2haar_features(path_read: str, path_write: str, n_f: int, n_img: int, img
     file.create_dataset('features', data=X)
     file.create_dataset('labels', data=y)
     file.close()
-    
-def read_hdf5(path_read: str):
-    #Read feature hdf5 file and return:
-    #X: features for each image
-    #y: labels for each image 
-    file = h5py.File(f'{path_read}', 'r')
-    X = file['features'] 
-    y = file['labels']
-    X = np.array(X)
-    y = np.array(y)
-    file.close()
-    return X, y
 
 if __name__ == "__main__":
     main()
