8a09b1846e60470b156a201facd6c09b825ba3d9
flenbob <ivan.flensburg@gmail.com>
2022-07-25 01:47:37 +0200

    changes

--- project 3/data/constants.py
+++ project 3/data/constants.py
@@ -2,10 +2,10 @@
 DELTA = 24
 SCALE = 2
 NEG_PATH_READ = '../negative_imgs'
-NEG_PATH_WRITE = 'project 3/data/neg_samples'
+NEG_PATH_WRITE = '../data'
 NEG_N_SAMPLES = 2000
 
 #READING POSITIVE SAMPLES
 POS_PATH_READ = '../positive_imgs'
-POS_PATH_WRITE = 'project 3/data/pos_samples'
+POS_PATH_WRITE = '../data'
 POS_N_SAMPLES = 1250
--- project 3/data/data.py
+++ project 3/data/data.py
@@ -4,7 +4,7 @@ import constants as c
 def run():
     #Get positive and negative samples saved to .pkl file
     pos_samples(c.POS_PATH_READ, c.POS_PATH_WRITE, c.POS_N_SAMPLES)
-    neg_samples(c.NEG_PATH_READ, c.NEG_PATH_WRITE, c.DELTA, c.SCALE, c.NEG_N_SAMPLES)
+    neg_samples(c.NEG_PATH_READ, c.NEG_PATH_WRITE, c.NEG_N_SAMPLES)
 
 if __name__ == "__main__":
     run()
